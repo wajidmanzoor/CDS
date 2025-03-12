355cf57b82f64f495f4dd62adc651b010c1dc821
wajidmanzoor <wajidmanzoor1999@gmail.com>
2025-03-12 14:32:21 -0400

    fixed bugs in code, testing in progress

--- cudaCode/inc/helpers.cuh
+++ cudaCode/inc/helpers.cuh
@@ -7,6 +7,15 @@
 // Kernel function declarations
 __global__ void generateDegreeDAG(deviceGraphPointers G, deviceDAGpointer D, ui *listingOrder, ui n, ui m, ui totalWarps);
 __global__ void generateNeighborDAG(deviceGraphPointers G, deviceDAGpointer D, ui *listingOrder, ui n, ui m, ui totalWarps);
+%%writefile helpers.cuh
+#pragma once
+
+#include "common.h"
+#include "graph.h"
+
+// Kernel function declarations
+__global__ void generateDegreeDAG(deviceGraphPointers G, deviceDAGpointer D, ui *listingOrder, ui n, ui m, ui totalWarps);
+__global__ void generateNeighborDAG(deviceGraphPointers G, deviceDAGpointer D, ui *listingOrder, ui n, ui m, ui totalWarps);
 __global__ void listIntialCliques(deviceDAGpointer D, cliqueLevelDataPointer levelData, ui *label, ui k, ui n, ui m, ui psize, ui cpSize, ui maxBitMask, ui level, ui totalWarps);
 __global__ void flushParitions(deviceDAGpointer D, cliqueLevelDataPointer levelData, ui psize, ui cpSize, ui k, ui maxBitMask, ui level, ui totalWarps);
 __global__ void listMidCliques(deviceDAGpointer D, cliqueLevelDataPointer levelData, ui *label, ui k, ui iterK, ui n, ui m, ui psize, ui cpSize, ui maxBitMask, ui totalTasks, ui level, ui totalWarps);
