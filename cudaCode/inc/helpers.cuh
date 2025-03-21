55c32dcaa4c4040e1b540e22005be0465b7233d6
wajidmanzoor <wajidmanzoor1999@gmail.com>
2025-03-21 13:11:32 -0400

    declare clique core decompose functions

--- cudaCode/inc/helpers.cuh
+++ cudaCode/inc/helpers.cuh
@@ -3,11 +3,19 @@
 #include "common.h"
 #include "graph.h"
 
-// Kernel function declarations
+// Kernel function to Generate DAG
 __global__ void generateDegreeDAG(deviceGraphPointers G, deviceDAGpointer D, ui *listingOrder, ui n, ui m, ui totalWarps);
 __global__ void generateNeighborDAG(deviceGraphPointers G, deviceDAGpointer D, ui *listingOrder, ui n, ui m, ui totalWarps);
+
+// Kernel function to List Cliques
 __global__ void listIntialCliques(deviceDAGpointer D, cliqueLevelDataPointer levelData, ui *label, ui k, ui n, ui m, ui psize, ui cpSize, ui maxBitMask, ui level, ui totalWarps);
 __global__ void flushParitions(deviceDAGpointer D, cliqueLevelDataPointer levelData, ui psize, ui cpSize, ui k, ui maxBitMask, ui level, ui totalWarps);
 __global__ void listMidCliques(deviceDAGpointer D, cliqueLevelDataPointer levelData, ui *label, ui k, ui iterK, ui n, ui m, ui psize, ui cpSize, ui maxBitMask, ui totalTasks, ui level, ui totalWarps);
 __global__ void writeFinalCliques(deviceGraphPointers G, deviceDAGpointer D, cliqueLevelDataPointer levelData, deviceCliquesPointer cliqueData, ui *globalCounter, ui k, ui iterK, ui n, ui m, ui psize, ui cpSize, ui maxBitMask, ui trieSize,ui totalTasks, ui level, ui totalWarps);
 __global__ void sortTrieData(deviceGraphPointers G, deviceCliquesPointer cliqueData, ui totalCliques, ui t, ui k, ui totalThreads);
+
+// Kernel function to Clique Core Decomposition
+__global__ void selectNodes(deviceGraphPointers G, ui *bufTails,ui *glBuffers, ui glBufferSize, ui n, ui level);
+__global__ void processNodesByWarp(deviceGraphPointers G,deviceCliquesPointer cliqueData, ui *bufTails,ui *glBuffers, ui *globalCount, ui glBufferSize, ui n, ui level, ui k);
+__global__ void processNodesByBlock(deviceGraphPointers G,deviceCliquesPointer cliqueData, ui *bufTails,ui *glBuffers, ui *globalCount, ui glBufferSize, ui n, ui level, ui k);
+
