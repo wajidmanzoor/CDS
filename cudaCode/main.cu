861664a3baa5fb362826265751f2992fe5b29f80
wajidmanzoor <wajidmanzoor1999@gmail.com>
2025-03-25 15:07:29 -0400

    restructured code

--- cudaCode/main.cu
+++ cudaCode/main.cu
@@ -5,31 +5,10 @@
 #include "./inc/gpuMemoryAllocation.cuh"
 #include "./inc/helpers.cuh"
 
-int main(int argc, const char * argv[]) {
-    if (argc != 7) {
-        cout << "Server wrong input parameters!" << endl;
-        exit(1);
-    }
 
-    string filepath = argv[1]; // Path to the graph file. The graph should be represented as an adjacency list with space separators
-    string motifPath = argv[2]; //Path to motif file. The motif should be represented as edge list with space sperators.
-    ui k = atoi(argv[3]);
-    ui pSize = atoi(argv[4]);
-    ui cpSize = atoi(argv[5]);
-    ui glBufferSize = atoi(argv[6]);
-
-    Graph graph = Graph(filepath);
-
-    //Motif M = Motif(motifPath);
-
-    vector<ui> listingOrder;
-    listingOrder.resize(graph.n);
-    graph.getListingOrder(listingOrder);
-
-    memoryAllocationGraph(deviceGraph, graph);
+void generateDAG(const Graph& graph,deviceGraphPointers& deviceGraph,deviceDAGpointer& deviceDAG , vector<ui> listingOrder){
+    
     memoryAllocationDAG(deviceDAG, graph.n, graph.m);
-
-    // THIS PART IS RELATED TO GENERATING DAG
 
     ui *listOrder;
     chkerr(cudaMalloc((void**)&(listOrder), graph.n * sizeof(ui)));
@@ -57,9 +36,9 @@ int main(int argc, const char * argv[]) {
 
     chkerr(cudaFree(listOrder));
 
+}
 
-    // THIS PART IS ABOUT CLIQUE LISTING ALGORITHM
-
+ui listAllCliques(const Graph& graph,deviceGraphPointers& deviceGraph,deviceDAGpointer& deviceDAG,cliqueLevelDataPointer levelData, ui k, ui pSize,ui  cpSize, ui t){
     thrust::device_ptr<ui> dev_degree(deviceDAG.degree);
 
 
@@ -123,7 +102,7 @@ int main(int argc, const char * argv[]) {
         level++;
     }
 
-    ui t = 10; // Make it ud
+    //ui t = 10; // Make it ud
     //TODO: decide the total number cliques and Free Level Data p 1
     chkerr(cudaFree(labels));    
     memoryAllocationTrie(cliqueData, t, k);
@@ -154,6 +133,125 @@ int main(int argc, const char * argv[]) {
     sortTrieData<<<BLK_NUMS, BLK_DIM,sharedMemorySort>>>(deviceGraph, cliqueData, tt,t, k, TOTAL_THREAD);
     CUDA_CHECK_ERROR("Sort Trie Data Structure");
 
+    cudaFree(totalCliques)
+    return tt;
+}
+
+void cliqueCoreDecompose(const Graph& graph,deviceGraphPointers& deviceGraph,deviceCliquesPointer& cliqueData, ui &maxCore, double &maxDensity, int &argmax, ui &coreSize, ui &coreTotalCliques, ui glBufferSize, ui k, ui t, ui tt){
+    ui level = 0;
+    ui count = 0;
+    ui *globalCount = NULL;
+    ui *bufTails  = NULL;
+    ui *glBuffers = NULL;
+
+    chkerr(cudaMalloc((void**)&(globalCount), sizeof(ui)));
+    chkerr(cudaMalloc((void**)&(bufTails), BLK_NUMS*sizeof(ui)));
+    chkerr(cudaMalloc((void**)&(glBuffers), BLK_NUMS*glBufferSize*sizeof(ui)));
+    chkerr(cudaMemset(globalCount, 0, sizeof(ui)));
+    cudaDeviceSynchronize();
+
+    chkerr(cudaMalloc((void**)&coreDensity, (graph.n+1)*sizeof(double)));
+
+    ui *removedVerticies, *remainingCliques;
+    chkerr(cudaMalloc((void**)&removedVerticies, (graph.n+1)*sizeof(ui)));
+    chkerr(cudaMalloc((void**)&remainingCliques, (graph.n+1)*sizeof(ui)));
+
+
+//    chkerr(cudaMemset(glBuffers, 0, BLK_NUMS*glBufferSize*sizeof(ui)));
+
+    chkerr(cudaMemcpy(deviceGraph.cliqueCore, deviceGraph.cliqueDegree, graph.n * sizeof(ui), cudaMemcpyDeviceToDevice));
+
+    //density of full graph
+    thrust::device_vector<ui> dev_vec1(cliqueData.status, cliqueData.status + t);
+    ui currentCliques = thrust::reduce(dev_vec1.begin(), dev_vec1.end(), 0, thrust::plus<ui>());
+    double d = static_cast<double>(currentCliques) / (graph.n - count);
+    chkerr(cudaMemcpy(coreDensity+level, &d, sizeof(double), cudaMemcpyHostToDevice));
+
+    while(count < graph.n){
+        cudaMemset(bufTails, 0, sizeof(unsigned int)*BLK_NUMS);
+
+        // Select nodes whoes current degree is level, that means they should be removed as part of the level core 
+        selectNodes<<<BLK_NUMS, BLK_DIM>>>(deviceGraph, bufTails, glBuffers, glBufferSize, graph.n, level);
+        cudaDeviceSynchronize();
+        
+        //Total number of verticies in buffer
+        thrust::device_vector<ui> dev_vec(bufTails, bufTails + BLK_NUMS);
+        ui sum = thrust::reduce(dev_vec.begin(), dev_vec.end(), 0, thrust::plus<ui>());
+
+        //Bases on total vertices device to either use Warp or Block to process one vertex and its cliques
+        if(true){
+            processNodesByWarp<<<BLK_NUMS, BLK_DIM>>>(deviceGraph, cliqueData , bufTails, glBuffers, globalCount, glBufferSize, graph.n, level, k, tt);
+            cudaDeviceSynchronize();
+        }else{
+            processNodesByBlock<<<BLK_NUMS, BLK_DIM>>>(deviceGraph, cliqueData , bufTails, glBuffers, globalCount, glBufferSize, graph.n, level, k, tt);
+            cudaDeviceSynchronize();
+
+        }
+
+        chkerr(cudaMemcpy(&count, globalCount, sizeof(unsigned int), cudaMemcpyDeviceToHost));    
+        level++;
+        currentCliques = thrust::reduce(dev_vec1.begin(), dev_vec1.end(), 0, thrust::plus<ui>());
+        d = static_cast<double>(currentCliques) / (graph.n - count);
+
+        chkerr(cudaMemcpy(coreDensity+level, &d, sizeof(double), cudaMemcpyHostToDevice));
+        chkerr(cudaMemcpy(removedVerticies+level , &count, sizeof(ui), cudaMemcpyHostToDevice));
+        chkerr(cudaMemcpy(remainingCliques+level , &currentCliques, sizeof(ui), cudaMemcpyHostToDevice));
+        cudaDeviceSynchronize();
+
+    }
+    graph.kmax = level-1;
+    maxCore = level-1;
+    thrust::device_ptr<double> coreDensity_ptr(coreDensity);
+    thrust::device_ptr<double> max_iter1 = thrust::max_element(coreDensity_ptr, coreDensity_ptr + level);
+    maxDensity = *max_iter1;
+    argmax = max_iter1 - coreDensity_ptr;
+
+    chkerr(cudaMemcpy(&coreSize, removedVerticies + argmax, sizeof(ui), cudaMemcpyDeviceToHost));
+    coreSize = graph.n - coreSize;
+    chkerr(cudaMemcpy(&coreTotalCliques, remainingCliques + argmax, sizeof(ui), cudaMemcpyDeviceToHost));
+
+    cudaFree(globalCount);
+    cudaFree(bufTails);
+    cudaFree(glBuffers);
+    cudaFree(coreDensity);
+    cudaFree(removedVerticies);
+    cudaFree(remainingCliques);
+
+}
+int main(int argc, const char * argv[]) {
+    if (argc != 7) {
+        cout << "Server wrong input parameters!" << endl;
+        exit(1);
+    }
+
+    string filepath = argv[1]; // Path to the graph file. The graph should be represented as an adjacency list with space separators
+    string motifPath = argv[2]; //Path to motif file. The motif should be represented as edge list with space sperators.
+    ui k = atoi(argv[3]);
+    ui pSize = atoi(argv[4]);
+    ui cpSize = atoi(argv[5]);
+    ui glBufferSize = atoi(argv[6]);
+
+    // Need better way to do this
+    ui t=10;
+
+    Graph graph = Graph(filepath);
+
+    //Motif M = Motif(motifPath);
+
+    vector<ui> listingOrder;
+    listingOrder.resize(graph.n);
+    graph.getListingOrder(listingOrder);
+
+    memoryAllocationGraph(deviceGraph, graph);
+
+    // GENERATES DAG
+    generateDAG(graph, deviceGraph, deviceDAG,listingOrder);
+
+    // CLIQUE LISTING ALGORITHM
+    ui tt = listAllCliques(graph, deviceGraph, deviceDAG, levelData, k, pSize, cpSize,t);
+
+
+    // Debug start 
     int *h_cliques,*status;
     h_cliques = new int[t*k];
     status = new int[t];
@@ -184,66 +282,16 @@ int main(int argc, const char * argv[]) {
         cout << h_cdegree[i] << " ";
     }
 
+    //Debug end
+
     //TODO:  CLIQUE CORE DECOMPOSE
+    ui coreSize, coreTotalCliques,maxCore;
+    double maxDensity;
+    int argmax;
+    cliqueCoreDecompose(graph,deviceGraph,cliqueData,maxCore, maxDensity, argmax, coreSize, coreTotalCliques,glBufferSize, k,  t, tt);
 
-    level = 0;
-    ui count = 0;
-    ui *globalCount = NULL;
-    ui *bufTails  = NULL;
-    ui *glBuffers = NULL;
 
-    chkerr(cudaMalloc((void**)&(globalCount), sizeof(ui)));
-    chkerr(cudaMalloc((void**)&(bufTails), BLK_NUMS*sizeof(ui)));
-    chkerr(cudaMalloc((void**)&(glBuffers), BLK_NUMS*glBufferSize*sizeof(ui)));
-    chkerr(cudaMemset(globalCount, 0, sizeof(ui)));
-    chkerr(cudaMalloc(&glBuffers,BLK_NUMS*glBufferSize*sizeof(ui)));
-    cudaDeviceSynchronize();
-
-    double *coreDensity;
-    chkerr(cudaMalloc((void**)&coreDensity, graph.n*sizeof(double)));
-
-//    chkerr(cudaMemset(glBuffers, 0, BLK_NUMS*glBufferSize*sizeof(ui)));
-
-    chkerr(cudaMemcpy(deviceGraph.cliqueCore, deviceGraph.cliqueDegree, graph.n * sizeof(ui), cudaMemcpyDeviceToDevice));
-
-    //density of full graph
-    thrust::device_vector<ui> dev_vec1(cliqueData.status, cliqueData.status + t);
-    ui currentCliques = thrust::reduce(dev_vec1.begin(), dev_vec1.end(), 0, thrust::plus<ui>());
-    double d = static_cast<double>(currentCliques) / (graph.n - count);
-    chkerr(cudaMemcpy(coreDensity+level, &d, sizeof(double), cudaMemcpyHostToDevice));
-
-    while(count < graph.n){
-        cudaMemset(bufTails, 0, sizeof(unsigned int)*BLK_NUMS);
-
-        // Select nodes whoes current degree is level, that means they should be removed as part of the level core 
-        selectNodes<<<BLK_NUMS, BLK_DIM>>>(deviceGraph, bufTails, glBuffers, glBufferSize, graph.n, level);
-        cudaDeviceSynchronize();
-        
-        //Total number of verticies in buffer
-        thrust::device_vector<ui> dev_vec(bufTails, bufTails + BLK_NUMS);
-        ui sum = thrust::reduce(dev_vec.begin(), dev_vec.end(), 0, thrust::plus<ui>());
-
-        //Bases on total vertices device to either use Warp or Block to process one vertex and its cliques
-        if(sum > 2* BLK_NUMS){
-            processNodesByWarp<<<BLK_NUMS, BLK_DIM>>>(deviceGraph, cliqueData , bufTails, glBuffers, globalCount, glBufferSize, graph.n, level, k, tt);
-            cudaDeviceSynchronize();
-        }else{
-            processNodesByBlock<<<BLK_NUMS, BLK_DIM>>>(deviceGraph, cliqueData , bufTails, glBuffers, globalCount, glBufferSize, graph.n, level, k, tt);
-
-        }
-
-        chkerr(cudaMemcpy(&count, globalCount, sizeof(unsigned int), cudaMemcpyDeviceToHost));    
-        level++;
-        if(count!=graph.n){
-            currentCliques = thrust::reduce(dev_vec1.begin(), dev_vec1.end(), 0, thrust::plus<ui>());
-            d = static_cast<double>(currentCliques) / (graph.n - count);
-            chkerr(cudaMemcpy(coreDensity+level, &d, sizeof(double), cudaMemcpyHostToDevice));
-
-        }
-        
-    }
-    graph.kmax = level-1;
-
+    
 
     //TODO: LOCATE CORE
 
